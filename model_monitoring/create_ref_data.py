1f34e415d2818865f5bb175147ff1bdcea3aeb07
matzatezalo <mateja.zatezalo@estudiantat.upc.edu>
2025-12-07 13:32:26 +0100

    Changed reference data

--- model_monitoring/create_ref_data.py
+++ model_monitoring/create_ref_data.py
@@ -5,7 +5,7 @@ reference_data = pd.DataFrame({
     'timestamp': pd.date_range(start='2024-01-01', periods=1000, freq='H'),
     'model': ['gender_model'] * 500 + ['authenticity_model'] * 500,
     'prediction': ['Male'] * 250 + ['Female'] * 250 + ['Real'] * 350 + ['Fake'] * 150, # 50% gender split, 70% real
-    'confidence': [0.7] * 1000
+    'confidence': [0.6] * 1000
 })
 
 reference_data.to_csv("model_monitoring/reference_data.csv", index=False)
